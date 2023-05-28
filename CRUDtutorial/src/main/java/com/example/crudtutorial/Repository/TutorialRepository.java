809374a07bd14d7bae0fdc7f6cac094cf25e5330
a38930 <quannguyen16902hn@gmail.com>
2023-05-28 17:37:16 +0700

    CRUD

---
+++ CRUDtutorial/src/main/java/com/example/crudtutorial/Repository/TutorialRepository.java
@@ -0,0 +1,22 @@
+package com.example.crudtutorial.Repository;
+
+import com.example.crudtutorial.entity.Tutorial;
+import jakarta.transaction.Transactional;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+
+
+@Repository
+@Transactional
+public interface TutorialRepository extends JpaRepository<Tutorial, Integer> {
+    List<Tutorial> findByTitleContainingIgnoreCase(String keyword);
+
+    @Query("UPDATE Tutorial t SET t.published = :published WHERE t.id = :id")
+    @Modifying
+    public void updatePublishedStatus(Integer id, boolean published);
+}
